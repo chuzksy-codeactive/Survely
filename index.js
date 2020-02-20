c65de366974f12eaf382a159716d547fc7ef79fa
onuchukwuchika <chuzksy@gmail.com>
2020-02-20 05:36:53 +0100

    add passport authentication using cookie session

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const key = require('./config/keys');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
+const keys = require('./config/keys');
 
 require('./models/User');
 require('./services/passport');
 
 const app = express();
 
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+)
+app.use(passport.initialize());
+app.use(passport.session());
 
-
-mongoose.connect(key.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (e) => { console.log('=============', e)})
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (e) => { console.log('=============', e)})
 
 require('./routes/authRoutes')(app);
 
