4e2b6aa63be4b5a2b334db00dd40e9397db39fad
onuchukwuchika <chuzksy@gmail.com>
2020-02-21 06:20:47 +0100

    setup combine reducers and setup react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './Header';
+
 
 const App = () => {
   return (
     <div>
-      Hi There
+      <Router>
+        <div>
+          <Header />
+        </div>
+      </Router>
     </div>
   )
 }
