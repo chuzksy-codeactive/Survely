9c2ae940f6c9ff5c0cd906eba53dcd1eab86dc1a
onuchukwuchika <chuzksy@gmail.com>
2020-02-25 07:57:07 +0100

    add stripe payment functionality to server

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const keys = require('./config/keys');
 
@@ -12,6 +13,7 @@ require('./services/passport');
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -24,6 +26,7 @@ app.use(passport.session());
 mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, (e) => { console.log(e) })
 
 require('./routes/authRoutes')(app);
+require('./routes/billingRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 
