898767cd9eecd45c693c35162c7ed307330c01d7
onuchukwuchika <chuzksy@gmail.com>
2020-02-25 10:51:45 +0100

    add production routing

--- index.js
+++ index.js
@@ -28,6 +28,14 @@ mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: tru
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
