13830a7d5dd757650d4acf440e32e9ce85928233
onuchukwuchika <chuzksy@gmail.com>
2020-02-26 08:02:51 +0100

    add survery schema and its sub document schema

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 
 require('./models/User');
+require('./models/Survey'); 
 require('./services/passport');
 
 const app = express();
