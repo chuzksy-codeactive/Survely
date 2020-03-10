45a5c5148f42519410bc82ae6c29fbea1f9f88a4
onuchukwuchika <chuzksy@gmail.com>
2020-03-10 14:01:40 +0100

    create email template and mailer objects

--- client/src/index.js
+++ client/src/index.js
@@ -4,11 +4,13 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxThunk from 'redux-thunk';
+import axios from 'axios';
 
 
 import App from './components/App';
 import reducers from './reducers/index';
 
+window.axios = axios;
 const store = createStore(reducers, {}, composeWithDevTools(applyMiddleware(reduxThunk)));
 
 ReactDom.render(
