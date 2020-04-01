6a3cc55c2373fe75d8c51663fcf9e49d3cc27151
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-01 11:16:16 +0100

    add api route for sendgrid event notification

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -38,4 +38,9 @@ module.exports = app => {
       res.status(422).send(err);
     }
   });
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    console.log(req.body);
+    res.send({});
+  });
 }
