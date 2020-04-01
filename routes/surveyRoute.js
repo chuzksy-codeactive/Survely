e1c7305a4725dada16a6d9f18c2b93582904a256
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-01 12:38:39 +0100

    webwook functionality implemented

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const _ = require('lodash');
+const { Path } = require('path-parser');
+const { URL } = require('url');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
 const Mailer = require('../services/Mailer');
@@ -40,7 +43,23 @@ module.exports = app => {
   });
 
   app.post('/api/surveys/webhooks', (req, res) => {
-    console.log(req.body);
-    res.send({});
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const path = new Path('/api/surveys/:surveyId/:choice');
+      const match = path.test(pathname);
+
+      if (match) {
+        return {
+          email,
+          surveyId: match.surveyId,
+          choice: match.choice
+        };
+      }
+
+      const compactEvents = _.compact(events);
+      const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId');
+
+      res.send({});
+    });
   });
 }
