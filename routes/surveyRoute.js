9547f6d55dc498e5b3c2a6aa44a5495b934037d2
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-01 18:05:48 +0100

    fetch surveys from database

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -14,6 +14,12 @@ module.exports = app => {
     res.send('Thanks for voting!');
   });
 
+  app.get('/api/surveys', requireLogin, async (req, res) => {
+    const surveys = await Survey.find({ _user: req.user.id });
+
+    res.send(surveys);
+  })
+
   app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
 
