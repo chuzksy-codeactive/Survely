abf410ed7812ff43d2535a59e814b127574b8fa0
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-01 18:22:33 +0100

    wiring survey to redux

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,13 @@
 import { combineReducers } from 'redux';
 import { reducer as reduxForm } from 'redux-form';
 import authReducer from './authReducer';
+import surveysReducer from './surveysReducer';
+
 
 const reducers = combineReducers({
   auth: authReducer,
-  form: reduxForm
+  form: reduxForm,
+  surveys: surveysReducer
 });
 
 export default reducers;
