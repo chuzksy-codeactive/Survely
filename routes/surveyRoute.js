9a7618ede9ee74889fc3fc2aa21bd0ecb9cf3e7e
chuzksy-codeactive <chuzksy@gmail.com>
2020-04-01 15:23:17 +0100

    update survey after responding to a survey

--- routes/surveyRoute.js
+++ routes/surveyRoute.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/emailTemplate/surveyTemplate')
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks for voting!');
   });
 
@@ -45,7 +45,7 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const path = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const pathname = new URL(url).pathname;
         const match = path.test(pathname);
@@ -60,9 +60,19 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne({
+          _id: surveyId,
+          recipients: {
+            $elemMatch: { email, responded: false }
+          }
+        }, {
+          $inc: { [choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }).exec();
+      })
       .value();
-
-    console.log(events, '===============');
 
     res.send({});
   });
